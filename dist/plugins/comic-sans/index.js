7254641ec0b56e4399935ed336ee98812e9e9df6
Claudio Condor <claucondor@gmail.com>
2025-01-15 15:18:40 -0400

    Update gitignore and clean cache

--- dist/plugins/comic-sans/index.js
+++
@@ -1,9 +0,0 @@
-import { comicSansAction } from "./action.js";
-export const comicSansPlugin = {
-    name: "comic-sans",
-    description: "Comic Sans detection and token rewards",
-    actions: [comicSansAction],
-    evaluators: [],
-    providers: []
-};
-export default comicSansPlugin;
--- dist/plugins/wallet/index.js
+++
@@ -1,9 +0,0 @@
-import { walletAction } from "./action.js";
-export const walletPlugin = {
-    name: "wallet",
-    description: "Wallet operations and token management",
-    actions: [walletAction],
-    evaluators: [],
-    providers: []
-};
-export default walletPlugin;
